d496cb642d9f5e05a444cad63e2768a1cef437df
Liz <lizriderwilson@gmail.com>
2021-03-22 13:41:02 -0400

    Remove controller behavior in order to redo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,3 @@
 class UsersController < ApplicationController
 
-    get '/signup' do
-        erb :"users/new"
-    end
-
-    post '/signup' do
-        "My username is #{params[:username]} and my email is #{params[:email]}"
-        redirect to '/users/:id'
-    end
-
-    get '/users/:slug' do
-        @user = User.all.find_by_slug(params[:slug])
-        erb :"users/show"
-    end
-
 end
