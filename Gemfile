dc589ec852b9802a2bfd07b750bdc484708a7561
Liz <lizriderwilson@gmail.com>
2021-04-03 15:02:19 -0400

    Add postgresql

--- Gemfile
+++ Gemfile
@@ -3,11 +3,15 @@ source 'http://rubygems.org'
 gem 'sinatra'
 gem 'activerecord'
 gem 'sinatra-activerecord', :require => 'sinatra/activerecord'
+gem 'pg'
 gem 'rake'
 gem 'require_all'
-gem 'sqlite3'
 gem 'thin'
 gem 'shotgun'
 gem 'pry'
 gem 'bcrypt'
 gem 'tux'
+
+group :development do
+    gem 'sqlite3'
+end
--- config/environment.rb
+++ config/environment.rb
@@ -3,11 +3,22 @@ ENV['SINATRA_ENV'] ||= "production"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-# if ENV['SINATRA_ENV'] == "development"
-#     require_relative "../session"
-# end
+if ENV['SINATRA_ENV'] == "development"
+    require_relative "../session"
+end
 
-set :database, {:adapter =>'sqlite3', :database=>"db/#{ENV['SINATRA_ENV']}.sqlite"}
+configure :production do
+    db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+ 
+    ActiveRecord::Base.establish_connection(
+      :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+      :host     => db.host,
+      :username => db.user,
+      :password => db.password,
+      :database => db.path[1..-1],
+      :encoding => 'utf8'
+      )
+ end
 
 require './app/controllers/application_controller'
 require_all 'app'
